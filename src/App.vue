fe28e2f564391e80d0a5e4679552c81eadf4eee4
Branham Karanja <branham.karanja@outlook.com>
2024-11-27 14:40:26 +0300

    Completed product quantity computation functionality

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,56 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="mt-8 mb-8 w-11/12 m-auto">
+    <h1 class="text-5xl font-bold w-11">Deserts</h1>
   </header>
-
-  <RouterView />
+  <main>
+    <section class="w-11/12 m-auto">
+      <ProductCard
+        v-for="(item, index) in productList"
+        :key="item.name"
+        :thumbnail="item.image.thumbnail"
+        :mobile="item.image.mobile"
+        :tablet="item.image.tablet"
+        :desktop="item.image.desktop"
+        :name="item.name"
+        :category="item.category"
+        :price="item.price"
+        :quantity="item.quantity"
+        :index="index"
+        :add="addQuantity"
+        :increment="increment"
+        :decrement="decrement"
+      />
+    </section>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+// import IconPic from './components/IconPic.vue'
+import ProductCard from './components/ProductCard.vue'
+import data from '../data.json'
+export default {
+  components: {
+    // IconPic,
+    ProductCard,
+  },
+  data() {
+    return {
+      productList: data,
+    }
+  },
+  methods: {
+    addQuantity(item) {
+      this.productList[item].quantity = 1
+    },
+    increment(item) {
+      this.productList[item].quantity++
+    },
+    decrement(item) {
+      this.productList[item].quantity--
+    },
+  },
+  mounted() {
+    // this.incrementQuantity()
+  },
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <article class="">
+    <div class="max-w-fit">
+      <figure class="mb-0">
+        <img :src="mobile" alt="dessert" class="rounded-lg" />
+        <figcaption></figcaption>
+      </figure>
+      <button
+        class="w-fit m-auto border border-rose_400 rounded-full px-6 py-2 font-medium bg-white relative -top-5 flex justify-center items-center gap-2 hover:shadow-lg"
+        v-if="!quantity"
+        @click="addQty"
+      >
+        <IconPic icon="cart" /> <span class="block">Add to Cart</span>
+      </button>
+      <div
+        class="w-36 m-auto py-1.5 rounded-full font-medium bg-red relative -top-5 flex justify-around items-center gap-2 hover:shadow-lg mb-1.5"
+        v-if="quantity"
+      >
+        <button @click="decrementQty"><IconPic icon="decrement" /></button>
+        <span class="block text-white">{{ quantity }}</span>
+        <button @click="incrementQty"><IconPic icon="increment" /></button>
+      </div>
+      <div>
+        <p class="text-rose_400 text-sm">{{ category }}</p>
+        <h2 class="text-rose_900 font-medium">{{ name }}</h2>
+        <p class="text-red font-medium">${{ price }}</p>
+      </div>
+    </div>
+  </article>
+</template>
+<script>
+import IconPic from './IconPic.vue'
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  components: {
+    IconPic,
+  },
+  props: [
+    'thumbnail',
+    'mobile',
+    'tablet',
+    'desktop',
+    'name',
+    'category',
+    'price',
+    'add',
+    'quantity',
+    'index',
+    'increment',
+    'decrement',
+  ],
+  computed: {},
+  methods: {
+    addQty() {
+      this.add(this.index)
+    },
+    incrementQty() {
+      this.increment(this.index)
+    },
+    decrementQty() {
+      this.decrement(this.index)
+    },
+  },
+  mounted() {},
+}
+</script>
